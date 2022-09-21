5df598dce65089aec0f64e6158abde0a220b3189
goziecodes <ezechukeuchigozie@gmail.com>
2022-09-21 05:56:55 +0100

    chaged nestor url to live

--- pages/api/transaction.js
+++ pages/api/transaction.js
@@ -60,7 +60,8 @@ export default async function handler(req, res) {
     const result = await axios.post(
       // "https://api.globaldatacompany.com/verifications/v1/verify",
       //  "https://tmp-msia-appgw.azure-api.net/nestor/trulio/sdkVerify",
-   "http://localhost:3000/trulio/sdkverifyasync",
+       "https://tmp-msia-appgw.azure-api.net/nestor/trulio/sdkverifyasync",
+  //  "http://localhost:3000/trulio/sdkverifyasync",
    req.body,
     );
     console.log(result, 'res me')
